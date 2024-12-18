8427afc50046225b1a4c81ce5f67e04eeae6528f
7account <DAvidliancongc2789@nycstudents.net>
2024-12-18 14:29:09 +0000

    first commit

---
+++ BoxOfCandy.java
@@ -0,0 +1,10 @@
+public class BoxOfCandy 
+{
+    private Candy[][] box;
+    public boolean moveCandyToFirstRow(int col) {
+        return null;
+    }
+    public Candy removeNextByFlavor(String flavor) {
+        
+    }
+}
